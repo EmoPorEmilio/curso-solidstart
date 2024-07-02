5b0934d5253a7e21988993d858ad261d4069bac3
Emilio Franceschini <franceschini.emilio@gmail.com>
2024-07-02 16:17:15 -0300

    jost font, layout, lateral nav

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,22 @@
 import { Router } from '@solidjs/router';
 import { FileRoutes } from '@solidjs/start/router';
 import { Suspense } from 'solid-js';
+import Header from '~/components/Header';
+import LateralNav from '~/components/LateralNav';
 import './app.css';
 
 export default function App() {
   return (
     <Router
       root={(props) => (
-        <main>
-          <Suspense>{props.children}</Suspense>
+        <main class='bg-bg-100 m-0 h-screen'>
+          <Header />
+          <div class='flex h-full bg-bg-100'>
+            <LateralNav />
+            <Suspense>{props.children}</Suspense>
+
+            <LateralNav transparent={true} />
+          </div>
         </main>
       )}>
       <FileRoutes />
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,18 +1,23 @@
 // @refresh reload
-import { createHandler, StartServer } from "@solidjs/start/server";
+import { createHandler, StartServer } from '@solidjs/start/server';
 
 export default createHandler(() => (
   <StartServer
     document={({ assets, children, scripts }) => (
-      <html lang="en">
+      <html lang='en'>
         <head>
-          <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          <meta charset='utf-8' />
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <link rel='icon' href='/favicon.ico' />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&family=Sen:wght@400..800&display=swap'
+            rel='stylesheet'
+          />
           {assets}
         </head>
         <body>
-          <div id="app">{children}</div>
+          <div id='app'>{children}</div>
           {scripts}
         </body>
       </html>
